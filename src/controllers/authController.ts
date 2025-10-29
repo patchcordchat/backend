114e088d7afdbdf46cbd6f51efd79d09d6125cdb
vlkosh <pshift4180@gmail.com>
2025-10-29 17:16:52 +0300

    feat: Реализованы методы-заглушки контроллера аутентификации и обновлены маршруты для сеансов пользователя, входа в систему, регистрации, управления паролями и восстановления учетной записи.

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,3 +1,113 @@
 import { Request, Response, NextFunction } from 'express';
-import User from '@/models/user';
 
+export const getSessions = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logoutSessions = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const login = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const register = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const registerByPhone = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const validatePasswordStrength = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const logout = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const forgotPassword = (req: Request, res: Response, next: NextFunction) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const resetPassword = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const revertAccount = (
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  try {
+    res.json({ message: 'Success' });
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -1,23 +1,32 @@
 import express from 'express';
 import {
-
+  getSessions,
+  logoutSessions,
+  login,
+  register,
+  registerByPhone,
+  validatePasswordStrength,
+  logout,
+  forgotPassword,
+  resetPassword,
+  revertAccount,
 } from '@/controllers/authController';
 
 const route = express.Router();
 
-route.get('/sessions'); // Get Auth Sessions
-route.post('/sessions/logout'); // Logout Auth Sessions
+route.get('/sessions', getSessions); // Get Auth Sessions
+route.post('/sessions/logout', logoutSessions); // Logout Auth Sessions
 
-route.post('/login'); // Login Account
-route.post('/register'); // Register Account
+route.post('/login', login); // Login Account
+route.post('/register', register); // Register Account
 
-route.post('/register/phone'); // Register Account with Phone Number
-route.post('/password/validate'); // Get Password Strength
+route.post('/register/phone', registerByPhone); // Register Account with Phone Number
+route.post('/password/validate', validatePasswordStrength); // Get Password Strength
 
-route.post('/logout'); // Logout
-route.post('/forgot'); // Forgot Password
-route.post('/reset'); // Reset Password
+route.post('/logout', logout); // Logout
+route.post('/forgot', forgotPassword); // Forgot Password
+route.post('/reset', resetPassword); // Reset Password
 
-route.post('/revert'); // Revert Account
+route.post('/revert', revertAccount); // Revert Account
 
 export default route;
