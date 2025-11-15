a162c488b3790cc428eaa0d0cc6390967600d0f2
vlkosh <pshift4180@gmail.com>
2025-11-16 00:15:07 +0300

    feat: добавлена поддержка Socket.IO для обработки веб-сокетов и обновлены зависимости в package.json и package-lock.json

---
+++ src/socket.ts
@@ -0,0 +1,11 @@
+import { Server as SocketIOServer } from 'socket.io';
+import { type Server } from 'http';
+
+export function initialize(server: Server): void {
+    const io = new SocketIOServer(server);
+
+    io.on('connection', (client) => {
+    })
+
+    console.log('Socket.IO server initialized');
+}
